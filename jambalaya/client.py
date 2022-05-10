56be71451837884ae6007d923b5b9e5d76fb5047
Jacob Deery <jacob.deery@gmail.com>
2022-05-09 22:36:03 -0400

    Remove playlist + track limits
    Previously, song lists longer than 100 items (whether from source
    playlists or as target playlist items) would cause issues, and if the
    user had more than 50 playlists, there was a chance the target playlist
    would not be found.

--- jambalaya/client.py
+++ jambalaya/client.py
@@ -1,3 +1,5 @@
+import math
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -23,17 +25,42 @@ class Client:
             return self._sp.track(uid)
 
     def get_playlist(self, name):
-        playlists = self._sp.current_user_playlists()  # TODO handle >50 playlists
-        for pl in playlists['items']:
-            if pl['name'] == name:
-                return pl
+        limit = 50
+        offset = 0
+        remaining = limit
+
+        while remaining > 0:
+            playlists = self._sp.current_user_playlists(limit=limit, offset=offset)
+            for pl in playlists['items']:
+                if pl['name'] == name:
+                    return pl
+            remaining = playlists['total'] - (offset + limit)
+            offset += limit
 
     def get_playlist_songs(self, name=None, uid=None):
         if name is not None:
             uid = self.get_playlist(name=name)['uri']
 
-        resp = self._sp.playlist_tracks(uid)
-        return [track['track'] for track in resp['items']]
+        tracks = []
+
+        limit = 100
+        offset = 0
+        remaining = limit
+
+        while remaining > 0:
+            resp = self._sp.playlist_tracks(uid, limit=limit, offset=offset)
+            tracks.extend([track['track'] for track in resp['items']])
+            remaining = resp['total'] - (offset + limit)
+            offset += limit
+
+        return tracks
 
     def set_playlist(self, pl_id, t_ids):
-        self._sp.playlist_replace_items(pl_id, t_ids)
+        self._sp.playlist_replace_items(pl_id, [])  # Clear the playlist
+
+        limit = 50
+
+        for i in range(math.ceil(len(t_ids) / limit)):
+            start_idx = i * limit
+            end_idx = min(start_idx + limit, len(t_ids))
+            self._sp.playlist_add_items(pl_id, t_ids[start_idx:end_idx])
